d284c0f60b2355c42e028317f96fd5a873f334ad
David <xiaopangzhichenwei@163.com>
2021-09-29 17:56:49 +0800

    2021/9/29

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/AllinChen/systool01/service"
 )
 
@@ -15,15 +18,15 @@ func main() {
 	// fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
 	// fmt.Println(service.GetInfo())
 
-	// for {
-	// 	if service.RecordInfo() == nil {
-	// 		fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "  Record Success")
-	// 		time.Sleep(10 * time.Second)
-	// 	}
-	// 	if fmt.Sprint(time.Now().Format("15:04:05")) == "08:00:00" {
-	// 		service.SendMail()
-	// 	}
-	// }
-	service.SendMail()
+	for {
+		if service.RecordInfo() == nil {
+			fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "  Record Success")
+			time.Sleep(10 * time.Second)
+		}
+		if fmt.Sprint(time.Now().Format("15:04:05")) == "08:00:00" {
+			service.SendMail()
+		}
+	}
+	// service.SendMail()
 
 }
